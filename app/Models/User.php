fd28e6c43bfb03ebbb62dc733d0f1b594d335d02
petrlobanov <lobanov@makeapp.ru>
2024-03-19 00:11:40 +0300

    Добавлены аннотации в модель user

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property int $age
+ * @property string $created_at
+ * @property string $updated_at
+ */
 class User extends Model
 {
     use HasFactory;
